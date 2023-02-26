5c44e47d0b9356226cca7798925b711a9b9f1689
phelipe-cyber <phelipegeraldo@hotmail.com>
2023-02-26 16:46:10 -0300

    Update export_recebido.php

--- export_recebido.php
+++ export_recebido.php
@@ -59,7 +59,7 @@ if (empty($_GET['dateini'])) {
             case WHEN recebido.pacote <> '' THEN recebido.data_hora ELSE producao.data_hora END 'Data Hora Atualizada',
             case WHEN recebido.pacote <> '' THEN recebido.usuario ELSE producao.usuario END 'Usuario Atualizado'
             FROM `db` as producao
-            LEFT OUTER JOIN `pcp_recebido` as recebido  on recebido.pacote = producao.pacote
+            LEFT OUTER JOIN `pcp_recebido` as recebido  on recebido.pacote = producao.pacote and recebido.deleted_at IS NULL
             WHERE producao.`data_hora` BETWEEN '$data 00:00:00' AND '$data 23:59:59' and producao.deleted_at IS NULL
              ORDER by `id` ASC");
             
@@ -128,7 +128,7 @@ if (empty($_GET['dateini'])) {
                         case WHEN recebido.pacote <> '' THEN recebido.data_hora ELSE producao.data_hora END 'Data Hora Atualizada',
                         case WHEN recebido.pacote <> '' THEN recebido.usuario ELSE producao.usuario END 'Usuario Atualizado'
                         FROM `db` as producao
-                        LEFT OUTER JOIN `pcp_recebido` as recebido  on recebido.pacote = producao.pacote
+                        LEFT OUTER JOIN `pcp_recebido` as recebido  on recebido.pacote = producao.pacote and recebido.deleted_at IS NULL
                         WHERE producao.`data_hora` BETWEEN '$Datainicio 00:00:00' AND '$Datafinal 23:59:59' and producao.deleted_at IS NULL
                          ORDER by `id` ASC");
                         
