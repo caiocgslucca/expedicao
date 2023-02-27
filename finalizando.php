fd5c1befcd921da0122c5d9a09629c6fa73ab410
phelipe-cyber <phelipegeraldo@hotmail.com>
2023-02-27 19:33:10 -0300

    Update finalizando.php

--- finalizando.php
+++ finalizando.php
@@ -9,7 +9,7 @@ date_default_timezone_set('America/recife');
 $datahora = (date('Y-m-d H:i:s'));
 
 $recebidos2 = ("SELECT case WHEN recebido.pacote <> '' THEN 'Recebido' ELSE 'Faltando Receber' END 'Status'
-FROM `db` as producao LEFT OUTER JOIN `pcp_recebido` as recebido on recebido.pacote = producao.pacote and recebido.deleted_at IS NULL WHERE producao.deleted_at IS NULL  
+FROM `db` as producao LEFT OUTER JOIN `pcp_recebido` as recebido on recebido.pacote = producao.pacote and recebido.finalizado IS NULL WHERE producao.finalizado IS NULL  
 group by `Status`");
 
 // $recebidos2 =("SELECT * FROM `testefull` WHERE `usuario` = '$usuario' AND `data_hora` BETWEEN '$datahorainicio' AND '$datahorafinal' ORDER BY `data_hora` DESC");                   
