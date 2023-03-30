d8822784d74197782f5887c8d4905a1e95dec98a
phelipe-cyber <phelipegeraldo@hotmail.com>
2023-03-30 20:47:16 -0300

    receber expedição e EAD

--- receber_pacote.php
+++ receber_pacote.php
@@ -11,18 +11,33 @@ $status = 1;
 
  $pacote_biper  =  $_POST['biper'];
 
- $result = ("SELECT db1.pedido, db1.pacote, db1.nota_fiscal, COUNT(db1.pedido) as qtde_item, 
-( SELECT COUNT(pedido) FROM `db` as db2 WHERE db2.pedido = db1.pedido and db2.finalizado is null and db2.deleted_at is null) as qtde_total_pedido,
-( SELECT COUNT(pr.pacote) from pcp_recebido pr where pr.pacote = db1.pacote and pr.finalizado is null and pr.deleted_at is null ) as pacote_recebido,
-( SELECT COUNT(pr.pedido) from pcp_recebido pr where pr.pedido = db1.pedido and pr.finalizado is null and pr.deleted_at is null ) as pedido_recebido
-FROM `db` as db1
-where db1.pacote = '$pacote_biper' and db1.finalizado is null and db1.deleted_at is null ");
+//  $result = ("SELECT db1.pedido, db1.pacote, db1.nota_fiscal, COUNT(db1.pedido) as qtde_item, 
+// ( SELECT COUNT(pedido) FROM `db` as db2 WHERE db2.pedido = db1.pedido and db2.finalizado is null and db2.deleted_at is null) as qtde_total_pedido,
+// ( SELECT COUNT(pr.pacote) from pcp_recebido pr where pr.pacote = db1.pacote and pr.finalizado is null and pr.deleted_at is null ) as pacote_recebido,
+// ( SELECT COUNT(pr.pedido) from pcp_recebido pr where pr.pedido = db1.pedido and pr.finalizado is null and pr.deleted_at is null ) as pedido_recebido
+// FROM `db` as db1
+// where db1.pacote = '$pacote_biper' and db1.finalizado is null and db1.deleted_at is null ");
+
+$result = ("SELECT producao.pacote, expedicao.*, expedicao.status as 'status_expedicao', producao.pedido, producao.nota_fiscal, COUNT(producao.pedido) as qtde_item,
+case WHEN recebido.pacote <> '' THEN 'Recebido' ELSE 'Faltando Receber ' END 'EAD Status',
+case WHEN expedicao.nro_etiqueta = producao.pacote THEN 'Na base Romaneio' ELSE 'Faltando na Base Romaneio' END 'Status Base Romaneio',
+case WHEN expedicao_recebido.nro_etiqueta <> '' THEN 'Recebido Romaneio' ELSE 'Faltando Receber Romaneio' END 'Romaneio Status',
+( SELECT COUNT(pedido) FROM `db` as db2 WHERE db2.pedido = producao.pedido and db2.finalizado is null and db2.deleted_at is null) as qtde_total_pedido,
+( SELECT COUNT(pr.pacote) from pcp_recebido pr where pr.pacote = producao.pacote and pr.finalizado is null and pr.deleted_at is null ) as pacote_recebido,
+( SELECT COUNT(pr.pedido) from pcp_recebido pr where pr.pedido = producao.pedido and pr.finalizado is null and pr.deleted_at is null ) as pedido_recebido
+FROM `db` as producao 
+LEFT OUTER JOIN `pcp_recebido` as recebido on recebido.pacote = producao.pacote and recebido.deleted_at IS NULL and recebido.finalizado IS NULL
+LEFT OUTER JOIN `expedicao` as expedicao on expedicao.nro_etiqueta = producao.pacote and expedicao.deleted_at IS NULL and expedicao.finalizado IS NULL
+LEFT OUTER JOIN `expedicao_recebido` as expedicao_recebido on expedicao_recebido.nro_etiqueta = expedicao.nro_etiqueta and expedicao_recebido.deleted_at IS NULL and expedicao_recebido.finalizado IS NULL
+WHERE producao.pacote = '$pacote_biper' and producao.deleted_at IS NULL and producao.finalizado IS NULL;");
 
 $recebidos = mysqli_query($conexao, $result);
 
 while ($rows = mysqli_fetch_assoc($recebidos)) {
 
             // print_r($rows);
+           
+        //  die();
             $pacote = $rows['pacote'];
             $_SESSION['pacote'] = $pacote;
             $pedido = $rows['pedido'];
@@ -31,6 +46,8 @@ while ($rows = mysqli_fetch_assoc($recebidos)) {
             $qtde_total_pedido = $rows['qtde_total_pedido'];
             $pedido_recebido = $rows['pedido_recebido'];
             $pacote_recebido = $rows['pacote_recebido'];
+            $Status_Base_Romaneio = $rows['Status Base Romaneio'];
+            
 
          if( $pacote == "" ){
               ?>
@@ -101,7 +118,6 @@ while ($rows = mysqli_fetch_assoc($recebidos)) {
                                             </div>
                                         </div>
                                         <div class="modal-footer d-flex justify-content-center">
-                                            <!-- <button submit class="btn btn-indigo">Voltar<i class="fas fa-paper-plane-o ml-1"></i></button> -->
                                         </div>
                                     </div>
                                 </div>
@@ -121,9 +137,6 @@ while ($rows = mysqli_fetch_assoc($recebidos)) {
 
                         if( $pacote_recebido >= 1 ){
                             ?>
-
-                                <!-- <form action="receber.php" method="POST"> -->
-
                                     <div class="modal fade" id="modalSubscriptionForm" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
                                         <div class="modal-dialog" role="document">
                                             <div class="modal-content">
@@ -145,12 +158,11 @@ while ($rows = mysqli_fetch_assoc($recebidos)) {
                                                     </div>
                                                 </div>
                                                 <div class="modal-footer d-flex justify-content-center">
-                                                    <!-- <button submit class="btn btn-indigo">Voltar<i class="fas fa-paper-plane-o ml-1"></i></button> -->
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
-                                <!-- </form> -->
+                                
                                 <script>
                                     var senha = 0;
 
@@ -162,13 +174,43 @@ while ($rows = mysqli_fetch_assoc($recebidos)) {
                                  echo '<meta http-equiv="refresh" content="1;URL=receber" />';
                                  die();
                         }else{
-                            $obs = strtoupper($_POST['observacao']);
+
+                            if( $Status_Base_Romaneio == "Na base Romaneio" ){
+
+                                $obs = strtoupper($_POST['observacao']);
+
+                                $nro_documento = $rows['nro_documento'];
+                                $nro_pedido = $rows['nro_pedido'];
+                                $nro_etiqueta = $rows['nro_etiqueta'];
+                                $dt_recebimento = $rows['dt_recebimento'];
+                                $nro_romaneio_expedicao = $rows['nro_romaneio_expedicao'];
+                                $qtde_volumes = $rows['qtde_volumes'];
+                                $qtde_itens = $rows['qtde_itens'];
+                                $status_expedicao = $rows['status_expedicao_expedicao'];
+                                $unidade_atual = $rows['unidade_atual'];
+                                $nome_pessoa_visita = $rows['nome_pessoa_visita'];
+                    
+                                $insert_sql_expedicao = "INSERT INTO `expedicao_recebido`  VALUES 
+                                (null, '$nro_documento', '$nro_pedido', '$nro_etiqueta', '$dt_recebimento', '$nro_romaneio_expedicao',
+                                 '$qtde_volumes', '$qtde_itens', '$status', '$unidade_atual', '$nome_pessoa_visita',  null, 
+                                 '$usuario', '$datahora',  null, '$obs')";
+                            $salvar = mysqli_query($conexao, $insert_sql_expedicao);
+
                             $insert_sql = "INSERT INTO `pcp_recebido`(`id`, `pacote`, `pedido`, `nota_fiscal`, `usuario`, `data_hora`, `deleted_at`, `finalizado`, `obs`) VALUES 
+                                            (NULL, '$pacote', '$pedido', '$nota_fiscal', '$usuario', '$datahora',null,null,'$obs')";
+                                        $salvar = mysqli_query($conexao, $insert_sql);
+                                        
+                                        echo '<meta http-equiv="refresh" content="0;URL=etiqueta" />';
+                                die();
+                            }else{
+                                
+                                $obs = strtoupper($_POST['observacao']);
+                                $insert_sql = "INSERT INTO `pcp_recebido`(`id`, `pacote`, `pedido`, `nota_fiscal`, `usuario`, `data_hora`, `deleted_at`, `finalizado`, `obs`) VALUES 
                                                 (NULL, '$pacote', '$pedido', '$nota_fiscal', '$usuario', '$datahora',null,null,'$obs')";
                                             $salvar = mysqli_query($conexao, $insert_sql);
-
-                                            echo '<meta http-equiv="refresh" content="0;URL=etiqueta" />';
                                             
+                                            echo '<meta http-equiv="refresh" content="0;URL=etiqueta" />';
+                            }
                                             
                         }
 
